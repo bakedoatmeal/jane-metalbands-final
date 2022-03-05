d5f8bc51721d85965cd854e95740a3dd695021cc
Jane <jane.anqi.cui@gmail.com>
2022-03-04 19:11:46 -0500

    stretch challenges 2,3,4

--- src/components/Band.js
+++ src/components/Band.js
@@ -3,14 +3,29 @@ import Like from './Like'
 import './Band.css'
 
 const Band = ({band}) => {
+
+  const getFans = (fanNumber) => {
+    return (fanNumber * 1000).toLocaleString('en')
+  }
+
+  const allStyles = band.style.split(',')
+  const styles = allStyles.map((style) => {
+    return (
+      <li>{style}</li>
+    )
+  })
+
   return (
     <div className='Band'>
       <p className='band-name'>{band.band_name}</p>
       <div className='band-info'>
-        <p>Formed: {band.formed} {band.origin} Fans: {band.fans} Split: {band.split}
+        <p>Formed: {band.formed} {band.origin} Fans: {getFans(band.fans)} Split: {band.split}
         </p>
       </div>
 
+      <ul>
+        {styles}
+      </ul>
       {band.split === "-" ? <Like/> : ''}
     
     </div>
--- src/components/MetalMeta.js
+++ src/components/MetalMeta.js
@@ -3,10 +3,19 @@ import react from 'react'
 
 const MetalMeta = () => {
   const bands = data
+
+  let totalFans = 0
+  for (let i=0; i < bands.length; i = i + 1) {
+    totalFans += bands[i].fans
+  }
+
+  const formattedFans = (totalFans * 1000).toLocaleString('en');
+
   return (
     <>
       <p>Metal Bands 🤘</p>
       <p>Bands: {data.length}</p>
+      <p>Total fans!! {formattedFans}</p>
     </>
   )
 }
